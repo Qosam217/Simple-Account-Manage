629bf888aaf6ed3812210c6a313b023bdd6bc7b0
Qosam217 <qosamimaduddin7@gmail.com>
2025-02-27 09:52:48 +0700

    setup project

---
+++ src/main/java/com/demo/simple_account/SimpleAccountApplication.java
@@ -0,0 +1,13 @@
+package com.demo.simple_account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleAccountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/simple_account/SimpleAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.simple_account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
